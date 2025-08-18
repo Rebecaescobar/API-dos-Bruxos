5c1301cd87db1d34a3bfa67b55ae41079901d98c
Rebeca <rebeca.escobar@aluno.senai.br>
2025-08-18 16:23:35 -0300

    finaliza atividade com json

---
+++ src/data/bruxos.js
@@ -0,0 +1,97 @@
+export default [
+        {
+            id: 1,
+            nome: "Harry Potter",
+            casa: "Grifinória",
+            ano: 7,
+            varinha: "Azevinho e pena de fênix, 28cm",
+            mascote: "Coruja (Edwiges)",
+            patrono: "Cervo",
+            especialidade: "Sobreviver a Voldemort 😎",
+        },
+        {
+            id: 2,
+            nome: "Hermione Granger",
+            casa: "Grifinória",
+            ano: 7,
+            varinha: "Videira e fibra de coração de dragão, 27cm",
+            mascote: "Gato (Bichento)",
+            patrono: "Lontra",
+            especialidade: "Saber tudo de tudo",
+        },
+        {
+            id: 3,
+            nome: "Ron Weasley",
+            casa: "Grifinória",
+            ano: 7,
+            varinha: "Salgueiro e crina de unicórnio, 35cm",
+            mascote: "Rato (Perebas)",
+            patrono: "Terrier Jack Russell",
+            especialidade: "Xadrez Bruxo",
+        },
+        {
+            id: 4,
+            nome: "Draco Malfoy",
+            casa: "Sonserina",
+            ano: 7,
+            varinha: "Espinheiro e crina de unicórnio, 25cm",
+            mascote: "Coruja (não nomeada)",
+            patrono: "Desconhecido",
+            especialidade: "Ser irritante",
+        },
+        {
+            id: 5,
+            nome: "Luna Lovegood",
+            casa: "Corvinal",
+            ano: 6,
+            varinha: "Madeira de cerejeira e núcleo desconhecido",
+            mascote: "Nenhum",
+            patrono: "Lebre",
+            especialidade: "Ver criaturas imaginárias (ou não) 👻",
+        },
+        {
+            id:6,
+            nome: "Minerva",
+            casa: "Grifinória",
+            ano: 6,
+            varinha: "Abeto e fibra de coração de dragão",
+            mascote: "Gato",
+            patrono: "Fenix",
+            Especialidade: "Transfiguração",
+        },
+        {
+            id:7,
+            nome: "Neville",
+            casa: "Grifinória",
+            ano: 6,
+            varinha: "Cereijeira e pelo de unicónio",
+            mascote: "Nenhum",
+            patrono: "Nenhum",
+            Especialidade: "Herbologia",
+        },
+        {
+            id:8,
+            nome: "Bellatrix",
+            casa: "Sonserina",
+            ano: 6,
+            varinha: "Fibra de coração de dragão",
+            mascote: "Rato",
+            patrono: "Dragão",
+            Especialidade: "Soltar fogo",
+        },
+        {
+            id:9,
+            nome: "Dollores",
+            casa: "Sonerina",
+            ano: 6,
+            varinha: "Vidoeiro",
+            mascote: "Não tem",
+            patrono: "Não tem",
+            Especialidade: "Guarda-caça"
+        }
+]
+          
+
+
+
+           
